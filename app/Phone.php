a605d75eaebc697b25f398fe1238ca52f5aaba70
aranna00 <aranna00@gmail.com>
2015-05-08 11:53:02 +0200

    undo model deletion

---
+++ app/Phone.php
@@ -0,0 +1,20 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Phone extends Model {
+
+	protected $table = 'phones';
+	protected $guarded = [];
+
+	public function spec()
+	{
+		return $this->hasMany('App\Spec');
+	}
+
+	public function contract()
+	{
+		return $this->belongsTo('App\Contract', 'id', 'phone_id');
+	}
+
+}
